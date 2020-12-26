43c110d9eb2b0fcf6ee0199c575632600f9187c0
Emmanuils Borovikovs <eb18847@essex.ac.uk>
2020-12-26 14:54:00 +0000

    Added bootstrap, created basic home page.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
 </template>
 
 <style>
+html, body {
+  height: 100%;
+}
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="home">
+  <div class="container-fluid vh-100">
+    <div class="row p-4">
+      <h1 class="display-2">Anonymous Rant</h1>
+    </div>
+    <div class="row mt-5 justify-content-center">
+      <div class="col-8">
+        <h4>
+          DFor writers, a random sentence can help them get their creative juices flowing. Since the topic of the sentence is completely unknown, it forces 
+        </h4>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import 'bootstrap/dist/css/bootstrap.css'
+
 export default {
-  name: 'Home',
-  components: {
-  }
+  name: 'Home'
 }
 </script>
