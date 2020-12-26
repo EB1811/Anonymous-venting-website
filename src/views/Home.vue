5cacfaa4a6bdd42322ef3c53aecdcc5c90aef4fa
Emmanuils Borovikovs <eb18847@essex.ac.uk>
2020-12-26 15:14:37 +0000

    Skeleton home page structure complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="container-fluid vh-100">
     <div class="row p-4">
-      <h1 class="display-2">Anonymous Rant</h1>
+      <h1 class="display-1" style="font-weight: 500">The Void</h1>
     </div>
-    <div class="row mt-5 justify-content-center">
-      <div class="col-8">
-        <h4>
+    <div class="row m-5 justify-content-center">
+      <div class="col-xs-12 col-md-8">
+        <h5 style="font-weight: 300">
           DFor writers, a random sentence can help them get their creative juices flowing. Since the topic of the sentence is completely unknown, it forces 
-        </h4>
+        </h5>
       </div>
+    </div>
+    <div class="row justify-content-center align-items-center h-25">
+      <div class="col">
+        <button type="button" class="btn btn-dark btn-lg">Large button</button>
+      </div>
+    </div>
+
+    <div>
+      
     </div>
   </div>
 </template>
