8a2d71f82a24d8125fd04fc1aa6074d9de1220ae
Emmanuils Borovikovs <eb18847@essex.ac.uk>
2020-12-27 11:20:36 +0000

    Installed firebase, added logic to create page

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="container-fluid vh-100">
     <div class="row justify-content-center align-items-center vh-100">
-      <div class="col-xs-12 col-sm-10 col-md-7 col-lg-6 col-xl-5 col-xxl-4" style="text-align: left">
-        <form class="p-5" style="background-color: rgba(0, 0, 0, 0.2)">
+      <div class="col-xs-12 col-sm-10 col-md-7 col-lg-6 col-xl-5 col-xxl-4" style="text-align: left; max-width: 500px">
+        <form class="p-5" style="background-color: rgba(0, 0, 0, 0.2)" @submit.prevent="handleSubmit">
           <div class="mb-3">
             <label class="form-label">Rant Title</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input type="text" class="form-control" required v-model="title">
           </div>
           <div class="mb-3">
             <label class="form-label">Rant Text</label>
-            <textarea class="form-control" rows="5"></textarea>
+            <textarea class="form-control" rows="8" required v-model="text"></textarea>
           </div>
           <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <input type="checkbox" class="form-check-input" v-model="isCommentsAllowed">
             <label class="form-check-label">Allow comments</label>
           </div>
           <div style="text-align: center">
@@ -26,7 +26,19 @@
 
 <script>
 export default {
-    
+  data() {
+    return {
+      title: '',
+      text: '',
+      isCommentsAllowed: false
+    }
+  },
+  methods: {
+    handleSubmit() {
+
+      console.log(this.title + this.text + this.isCommentsAllowed)
+    }
+  }
 }
 </script>
 
